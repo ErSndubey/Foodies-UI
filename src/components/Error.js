ef2012f81e1f451730782b851780073ae7feb209
ErSnDubey <ersndubey@gmail.com>
2023-07-19 23:07:29 +0530

    Error router and the error data is added.

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,14 +1,27 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { useRouteError } from "react-router-dom";
 
-const error = () => {
-    const err = useRouteError;
-    console.log(err)
+const Error = () => {
+
+const err = useRouteError();
+console.log(err);
+
+  // Get the current location object from react-router-dom's useLocation hook
+  const location = useLocation();
+  
+  // Extract the error data from the location state, if it exists
+  const errorData = location.state?.error;
+
   return (
     <div>
       <h1>Oops!</h1>
-      <h2>Somthing wenr wrong!!</h2>
+      <h2>Something went wrong!!</h2>
+      <h2>{err.status+":"+ err.statusText}</h2>
+      {/* Render the error message if errorData exists */}
+      {errorData && <p>Error message: {errorData.message}</p>}
     </div>
   );
 };
 
-export default Error; 
+export default Error;
