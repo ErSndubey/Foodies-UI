1ddcea95e231d7fd4e7797f232a83d0d4f838ea7
ErSnDubey <ersndubey@gmail.com>
2023-08-05 05:38:42 +0530

    fix

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -63,7 +63,7 @@ const RestaurantMenu = () => {
                 <img
                   src={ITEM_IMG_CDN_URL + item?.imageId}
                   alt={item?.name}
-                  className="w-80 max-w-[8rem]  xl:w-32 md:w-32 object-cover rounded-md border border-gray-100"
+                  className="w-80 h-24 max-w-[8rem]  xl:w-32 md:w-32 object-cover rounded-md border border-gray-100"
                 />
                 <button
                   className=" w-20 px-1 xl:px-3 py-0.5 xl:py-1 border border-gray-400 bg-white text-xs text-green-600 font-bold rounded absolute bottom-0 left-1/2 transform -translate-x-1/2 -mb-3 hover:bg-green-500 hover:text-white hover:border-white"
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -25,10 +25,10 @@ export const getRestaurants = async (
     const json = await data.json();
     console.log(json);
     setRestaurants(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
     setFilteredRestaurants(
-      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
 
     return [setRestaurants, setFilteredRestaurants];
