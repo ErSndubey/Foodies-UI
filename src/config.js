076ff3c91984fabeab3b9cd00cf39c1b488c1dc1
ErSnDubey <ersndubey@gmail.com>
2023-08-08 15:15:52 +0530

    location api error fix.

--- src/config.js
+++ src/config.js
@@ -3,8 +3,27 @@ export const IMG_CDN_URL ="https://res.cloudinary.com/swiggy/image/upload/fl_los
 export const ResData_API_URL_DESKTOP =
   "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
-  export const ResData_API_URL_MOBILE =
-  "https://corsproxy.io/?https://https://www.swiggy.com/mapi/homepage/getCards?lat=12.9715987&lng=77.5945627";
+  // Construct the API URL using template literals
+export const ResData_API_URL_MOBILE = async () => {
+  try {
+    // Get the user's current location
+    const position = await new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(resolve, reject);
+    });
+
+    // Extract latitude and longitude from the position object
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    // Construct the API URL with dynamic latitude and longitude
+    return `https://corsproxy.io/?https://www.swiggy.com/mapi/homepage/getCards?lat=${latitude}&lng=${longitude}`;
+  } catch (error) {
+    console.error("Error fetching user's location:", error);
+    return null;
+  }
+};
+/*   export const ResData_API_URL_MOBILE =
+  "https://corsproxy.io/?https://www.swiggy.com/mapi/homepage/getCards?lat=19.0759837&lng=72.8776559"; */
 
   export const ITEM_IMG_CDN_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_208,h_208,c_fit/"; 
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -67,8 +67,4 @@ export const handleSearch = (
 
 //cart quantity control buttons
 
-/* mumbai */
-("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
 
-/* Banglore */
-("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
